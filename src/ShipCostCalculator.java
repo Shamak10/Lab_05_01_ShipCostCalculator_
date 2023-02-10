e7972494d276a1b2a9642b16974a2552c85e0aee
Shamak Patel <shamak@dhcp-172-29-17-10.guest.uc.edu>
2023-02-10 12:40:25 -0500

    Initial commit

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the price of the item: $200");
+        double shippingCost;
+        double price;
+
+        if(price<100);
+        {
+            System.out.println(shippingCost = price * 0.02);
+            System.out.println("Total price" + (price + (price * 0.02)));
+        }else{shippingCost= 0;
+            System.out.println("shippingCost is 0");
+            System.out.println("Total price" + price);
+}
+        System.out.println("Shipping cost: $" + shippingCost);
+        System.out.println("Total price: $" + (price + shippingCost));
+    }
+}
