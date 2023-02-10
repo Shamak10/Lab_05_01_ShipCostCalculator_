109fee5ca2bc929b9136ef2f3c2c794eebc66287
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-10 12:43:56 -0500

    Update ShipCostCalculator.java

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,19 +1,17 @@
-public class Main {
+public class ShipCostCalculator {
     public static void main(String[] args) {
-
-        System.out.println("Enter the price of the item: $200");
+        System.out.println("Enter the price of the item: $50"); //Asking the user to enter item price
         double shippingCost;
-        double price;
+        double price = 50;
 
-        if(price<100);
-        {
-            System.out.println(shippingCost = price * 0.02);
-            System.out.println("Total price" + (price + (price * 0.02)));
-        }else{shippingCost= 0;
-            System.out.println("shippingCost is 0");
-            System.out.println("Total price" + price);
-}
-        System.out.println("Shipping cost: $" + shippingCost);
-        System.out.println("Total price: $" + (price + shippingCost));
+        if (price < 100) {
+            shippingCost = price * 0.02; //calculating shippingCost
+            System.out.println("Shipping cost: $" + shippingCost);// outputs shippingCost
+            System.out.println("Total price: $" + (price + shippingCost));// outputs total price with the shipping cost
+        } else {
+            shippingCost = 0 ;// Shipping cost is 0
+            System.out.println("Shipping cost is 0"); // shipping cost is free as item price is above $100
+            System.out.println("Total price: $" + price);// outputs total price
+        }
     }
 }
